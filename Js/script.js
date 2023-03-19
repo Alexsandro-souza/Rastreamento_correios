6f245e8b10deaccd4d634bf219bc088af8601920
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-03-19 18:39:35 -0300

    alterando HTML com dados da response

--- Js/script.js
+++ Js/script.js
@@ -15,14 +15,26 @@ const clicando = botao.addEventListener("click", (e) => {
 //Concatenando protoloca com URL correios
 const getData = async () => {
    const valorProtoloco = protocolo.value.toUpperCase(); //O metedo toUpperCase() transforma em mauiscula toda string    
-    const apiURL = `https://api.linketrack.com/track/json?user=teste&token=1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f&codigo=${valorProtoloco}`;
-   
-    const resposta = await fetch(apiURL);
-    const data = await resposta.json();
-    return data;
+   const apiURL = `https://api.linketrack.com/track/json?user=teste&token=1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f&codigo=${valorProtoloco}`;
+   const resposta = await fetch(apiURL);
+   const data = await resposta.json();   
+   alterarHtml(data);
+   return data;
+      
 }
-
-
-
 //Alterando dados na HTML
+ function alterarHtml(data) {
+    const dia = data.eventos[0].data;
+    const hora = data.eventos[0].hora;
+    const local1 = data.eventos[0].local;
+    const status = data.eventos[0].status;
+    dataEvento.innerText = dia
+    horaEvento.innerText = hora
+    local.innerText = local1
+    eventoMensagem.innerText = status
+
+ };
+
+ //console.log(dia);
+     //alert('Cara! Sinto muito o campo protoloco está vazio 😥');
 
